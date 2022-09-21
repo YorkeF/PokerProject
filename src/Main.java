0277bd9bd2483929d714d532b0568d5bb5be43eb
YorkeF <113220697+YorkeF@users.noreply.github.com>
2022-09-20 19:32:59 -0500

    Poker Project

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        //this is a commit
-        //this is also a commit
-        //testing thing
     }
 }
